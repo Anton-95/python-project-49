d84407cbc001640a5b53078a0c67bf894519e33d
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-26 10:49:34 +0300

    fix linter errors

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def main():
     print('Welcome to the Brain Games!')
     welcome_user()
-    
+
 
 if __name__ == '__main__':
     main()

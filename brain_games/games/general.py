a1e98b912e93b6e2e44656e060e16564740d549c
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-27 22:56:25 +0300

    add general.py

---
+++ brain_games/games/general.py
@@ -0,0 +1,14 @@
+from prompt import string
+
+
+def question_answer(question):
+    answer = string(f'Question: {question}\nYour answer: ', empty=False)
+    return answer
+
+
+def wrong_answer(answer, correct_result, user_name):
+    print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_result}'. Let's try again, {user_name}!")
+
+
+def final(user_name):
+    print(f'Congratulations, {user_name}!')

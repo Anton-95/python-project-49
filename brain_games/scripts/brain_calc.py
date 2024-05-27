d607d350ad32cbdac57c70f4cbb90169180bd6af
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-27 23:00:14 +0300

    add brain_calc.py

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.calc import calculate
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    calculate()
+
+
+if __name__ == '__main__':
+    main()

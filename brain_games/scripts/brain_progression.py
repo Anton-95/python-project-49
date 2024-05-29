6bb8f20b216b24082000e6cc339222ed605d4d1b
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-29 21:37:33 +0300

    add brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.progression import defenition_number
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    defenition_number()
+
+
+if __name__ == '__main__':
+    main()

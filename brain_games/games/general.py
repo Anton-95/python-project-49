ed9c6019907c0281ea1c2f71ed95fb26bc951383
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-30 22:18:18 +0300

    fix general.py: fix def wrong_answer

--- brain_games/games/general.py
+++ brain_games/games/general.py
@@ -7,8 +7,8 @@ def question_answer(question):
 
 
 def wrong_answer(answer, correct_result, user_name):
-    print(f"'{answer}' is wrong answer ;(. Correct answer was "
-          "'{correct_result}'. Let's try again, {user_name}!")
+    print(f"'{answer}' is wrong answer ;(. Correct answer was \
+'{correct_result}'. Let's try again, {user_name}!")
 
 
 def final(user_name):

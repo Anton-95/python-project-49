437cce0f8d69b5fcda2590086d33459852395354
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-05-30 22:15:33 +0300

    add script brain_prime.py

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.prime import prime_number_definition
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    prime_number_definition()
+
+
+if __name__ == '__main__':
+    main()

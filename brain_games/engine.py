06cf3b70c4139242828392b1aa41d5dff32f0292
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-05 18:55:17 +0300

    move and mod engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,20 +1,27 @@
+from brain_games.cli import welcome_user
 from prompt import string
 
 
-def round(question, user_name):
+def game_launch(game_module):
+    user_name = welcome_user()
+    print(game_module.GAME_RULES)
+    looping_rounds(game_module.GENERATE_QUESTION_ANSWER, user_name)
+
+
+def looping_rounds(question, user_name):
     for _ in range(3):
         correct_answer_user = question()
-        result_cheking_answer = checking_answer(question_and_user_answer
+        result_cheking_answer = checking_answer(output_question_enter_answer
                                                 (correct_answer_user[0]),
                                                 correct_answer_user[1],
                                                 user_name)
         if result_cheking_answer == 'wrong':
             break
     else:
-        final(user_name)
+        print(f'Congratulations, {user_name}!')
 
 
-def question_and_user_answer(question):
+def output_question_enter_answer(question):
     user_answer = string(f'Question: {question}\nYour answer: ', empty=False)
     return user_answer
 
@@ -26,7 +33,3 @@ def checking_answer(user_answer, correct_answer, user_name):
         print(f"'{user_answer}' is wrong answer ;(. Correct answer was \
 '{correct_answer}'. Let's try again, {user_name}!")
         return 'wrong'
-
-
-def final(user_name):
-    print(f'Congratulations, {user_name}!')

4d2b08202ad4b4085f82627abf34432ec24226d0
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-06 19:36:12 +0300

    mod prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,9 +8,15 @@ def generate_question_answer():
     number = randint(5, 50)
     half_number = number // 2
     for divider in range(2, half_number):
-        if number % divider == 0:
+        if check_prime_number(number, divider):
             correct_answer = 'no'
             break
         else:
             correct_answer = 'yes'
     return (number, correct_answer)
+
+
+def check_prime_number(num, divider):
+    if num % divider == 0:
+        return True
+    return False

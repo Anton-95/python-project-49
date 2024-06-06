8113c4bee19022ec2eb7909fc9913ce693fcdec8
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-06 19:23:13 +0300

    mod even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,8 +6,14 @@ GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def generate_question_answer():
     number = randint(1, 100)
-    if number % 2 == 0:
+    if cheсk_parity_number(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return (number, correct_answer)
+
+
+def cheсk_parity_number(num):
+    if num % 2 == 0:
+        return True
+    return False

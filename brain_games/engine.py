9368bf6ec8b1952c190d6d31a310b0e2c7d135ee
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-08 21:49:14 +0300

    fix engine.py: deleted func

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,10 +8,12 @@ def game_launch(game_module):
     for _ in range(3):
         question, correct_answer = game_module.generate_question_answer()
         print(f'Question: {question}')
-        result_cheking_answer = checking_answer(enter_answer(),
-                                                correct_answer,
-                                                user_name)
-        if result_cheking_answer == 'wrong':
+        user_answer = enter_answer()
+        if user_answer == correct_answer:
+            print('Correct!')
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was \
+'{correct_answer}'. Let's try again, {user_name}!")
             break
     else:
         print(f'Congratulations, {user_name}!')
@@ -20,12 +22,3 @@ def game_launch(game_module):
 def enter_answer():
     user_answer = string('Your answer: ', empty=False)
     return user_answer
-
-
-def checking_answer(user_answer, correct_answer, user_name):
-    if user_answer == correct_answer:
-        print('Correct!')
-    else:
-        print(f"'{user_answer}' is wrong answer ;(. Correct answer was \
-'{correct_answer}'. Let's try again, {user_name}!")
-        return 'wrong'

09e55a1dbbecbab46016c92d37b662f15a798117
Anton Arkhipov <antonolegovich44@yandex.ru>
2024-06-08 21:50:20 +0300

    fix even.py: rename func

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,14 +6,14 @@ GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def generate_question_answer():
     number = randint(1, 100)
-    if cheсk_parity_number(number):
+    if is_even(number):
         correct_answer = 'yes'
     else:
         correct_answer = 'no'
     return (number, correct_answer)
 
 
-def cheсk_parity_number(num):
+def is_even(num):
     if num % 2 == 0:
         return True
     return False
